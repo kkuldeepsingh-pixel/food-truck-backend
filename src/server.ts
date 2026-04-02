c0cdcd545b900a349f316ddedbf76736a7b954db
Kuldeep Singh <kkuldeepsingh@academic.rrc.ca>
2026-04-01 20:06:08 -0500

    chore: connect reviews routes in server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
-import express from "express";
+import express, { Application } from "express";
 import cors from "cors";
-import reviewsRouter from "./routes/reviews";
+import reviewsRoutes from "./routes/reviews";
 
-const app = express();
+const app: Application = express();
+const PORT = 5000;
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use("/reviews", reviewsRouter);
+// Routes
+app.use("/reviews", reviewsRoutes);
 
-const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
