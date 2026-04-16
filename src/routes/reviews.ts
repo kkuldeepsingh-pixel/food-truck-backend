ea84c40df3de0b491557701aea01c0740875572c
Kuldeep Singh <kkuldeepsingh@academic.rrc.ca>
2026-04-16 18:33:41 -0500

    Completed authentication

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -7,21 +7,17 @@ import {
   deleteReview
 } from "../controllers/reviewsController";
 
+import { authenticate } from "../middleware/authMiddleware";
+
 const router = Router();
 
-// GET all reviews
+// PUBLIC ROUTES
 router.get("/", getAllReviews);
-
-// GET a single review by ID
 router.get("/:id", getReviewById);
 
-// POST a new review
-router.post("/", createReview);
-
-// PUT to update an existing review
-router.put("/:id", updateReview);
-
-// DELETE a review
-router.delete("/:id", deleteReview);
+// PROTECTED ROUTES
+router.post("/", authenticate, createReview);
+router.put("/:id", authenticate, updateReview);
+router.delete("/:id", authenticate, deleteReview);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import cors from "cors";
 import reviewsRoutes from "./routes/reviews";
 import menuRoutes from "./routes/menu";
 import rateLimit from "express-rate-limit";
+import jwt from "jsonwebtoken";
 
 const app: Application = express();
 const PORT = 5000;
+
 const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 10 requests
+  windowMs: 60 * 1000,
+  max: 2,
   message: "Too many requests, try again later",
 });
 
@@ -18,13 +20,26 @@ app.use(limiter);
 app.use(cors());
 app.use(express.json());
 
+// LOGIN ROUTE
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).json({ message: "Username is required" });
+  }
+
+  const token = jwt.sign({ username }, "mysecret", { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
 // Routes
 app.use("/reviews", reviewsRoutes);
+app.use("/menu", menuRoutes);
+
 app.get("/", (_req, res) => {
   res.send("Food Truck Backend API is running!");
 });
-
-app.use("/menu", menuRoutes);
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
